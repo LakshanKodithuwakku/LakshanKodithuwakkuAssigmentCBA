fb3a5484d1fa276d2995aa1e495b92cb2c48ce1b
Lakshan Kodithuwakku <63406685+LakshanKodithuwakku@users.noreply.github.com>
2023-06-23 12:14:08 +0530

    api connect

--- app/src/main/java/com/example/lakshankodithuwaku/MainActivity.java
+++ app/src/main/java/com/example/lakshankodithuwaku/MainActivity.java
@@ -6,9 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    TextView data;
+    String url;
 
     private Button login;
 
@@ -16,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data = findViewById(R.id.data);
+        url = "https://worldtimeapi.org/api/timezone/Asia/Colombo";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String datetime = response.getString("datetime");
+                    data.setText(datetime);
+                    System.out.println(datetime);
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
+
         login=findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
