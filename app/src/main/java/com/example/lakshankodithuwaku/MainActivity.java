5e45d1d4bf0f131e7280956eb46c52ee4087dea5
Lakshan Kodithuwakku <63406685+LakshanKodithuwakku@users.noreply.github.com>
2023-06-23 18:42:57 +0530

    view profile

--- app/src/main/java/com/example/lakshankodithuwaku/MainActivity.java
+++ app/src/main/java/com/example/lakshankodithuwaku/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.lakshankodithuwaku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -17,9 +20,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class MainActivity extends AppCompatActivity {
 
     TextView data;
+    EditText username;
+    EditText password;
     String url= "http://123.231.9.43:3999";
 
     private Button login;
@@ -30,14 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         data = findViewById(R.id.data);
-
-
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
         login=findViewById(R.id.login);
+
+        String testname = "Lakshan";
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                sendJsonRequest(url, "test", "Test12", new JsonRequestCallback() {
+                sendJsonRequest(url, username.getText().toString(), password.getText().toString(), new JsonRequestCallback() {
                     @Override
                     public void onResponseString(String response) {
                         System.out.println(response);
@@ -65,6 +73,18 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         String res_desc = response.getString("res_desc");
                         data.setText(res_desc);
+
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("SharedPref_CBA", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("id", response.getJSONObject("user_data").getString("id"));
+                        editor.putString("email", response.getJSONObject("user_data").getString("email"));
+                        editor.putString("name", response.getJSONObject("user_data").getString("name"));
+                        editor.putString("dob", response.getJSONObject("user_data").getString("dob"));
+                        editor.putString("gender", response.getJSONObject("user_data").getString("gender"));
+                        editor.putString("company", response.getJSONObject("user_data").getString("company"));
+                        editor.putString("position", response.getJSONObject("user_data").getString("position"));
+                        editor.apply();
 
                         // Invoke the callback with the response string
                         callback.onResponseString(res_desc);
--- app/src/main/java/com/example/lakshankodithuwaku/home.java
+++ app/src/main/java/com/example/lakshankodithuwaku/home.java
@@ -2,13 +2,37 @@ package com.example.lakshankodithuwaku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
-
+    TextView view;
+    private Button view_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        view = findViewById(R.id.data_view);
+        view_button = findViewById(R.id.button_view);
+
+        view_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("SharedPref_CBA", MODE_PRIVATE);
+                String id = sharedPreferences.getString("id", "");
+                String email = sharedPreferences.getString("email", "");
+                String name = sharedPreferences.getString("name", "");
+                String dob = sharedPreferences.getString("dob", "");
+                String gender = sharedPreferences.getString("gender", "");
+                String company = sharedPreferences.getString("company", "");
+                String position = sharedPreferences.getString("position", "");
+                view.setText("Id: "+ id +"\n"+ "email: " + email+"\n"+"Name: "+ name +"\n"+ "DOB: " + dob+"\n"+"Gender: "+
+                        gender +"\n"+ "Company: " + company+"\n"+"Position: "+ position);
+            }
+        });
     }
 }
